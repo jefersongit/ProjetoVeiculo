5d43aaf7cf44b92d46ec8822c4110328193a1889
augustowatzko <32853463+augustowatzko@users.noreply.github.com>
2018-11-13 21:27:12 -0200

    feita as classes veiculo

--- src/veiculo/dao/VeiculoDAO.java
+++ src/veiculo/dao/VeiculoDAO.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import veiculo.dao.Dao;
+import veiculo.dao.DbConnection;
 
 import veiculo.model.Veiculo;
 
--- src/veiculo/model/Veiculo.java
+++ src/veiculo/model/Veiculo.java
@@ -126,3 +126,4 @@ public class Veiculo {
         return false;
     }
 }
+
